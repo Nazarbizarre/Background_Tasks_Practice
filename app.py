adbc3647a7b455646ba9b68fccca2c492c4cbf04
Nazar Bezpalyi <nazar.bez007@gmail.com>
2024-11-21 20:34:34 +0100

    final commit

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI, File, UploadFile, BackgroundTasks, status, HTTPExce
 from time import sleep
 from pathlib import Path
 from sqlmodel import SQLModel, Field, create_engine, Session, select
-from uuid import uuid4
+import uuid
 import shutil
 from uvicorn import run as run_project
 
@@ -13,7 +13,7 @@ UPLOAD_DIR = Path("uploads")
 UPLOAD_DIR.mkdir(exist_ok=True)
 
 class FileModel(SQLModel, table=True):
-    id: str = Field(default_factory=lambda: str(uuid4()), primary_key=True)
+    id: str = Field(primary_key=True)
     name: str
     progress: int = Field(default=0)
     
@@ -28,56 +28,60 @@ def get_session():
     with Session(engine) as session:
         yield session
 
-def generate_progress_updates(filename: str = ""):
+def generate_progress_updates(file_id: str, db: Session):
+    file = db.exec(select(FileModel).where(FileModel.id == file_id)).first()
     for i in range(10):
-        yield f"Processing {filename}: {i * 10}% complete\n"
-        sleep(5)
-    yield f"Processing {filename}: 100% complete\n"
+        file.progress = i * 10 
+        sleep(2)
+    file.progress = 100
+    db.commit()
 
-async def process_file(file_path: Path, background_tasks = BackgroundTasks):
+async def process_file(file_path: Path, file_id: str, db: Session, background_tasks = BackgroundTasks):
 
-        
+    
+    
     if file_path.suffix != ".txt":
         raise HTTPException(status_code=422, detail="Failed: Unsupported file format, please upload a .txt file")
 
-    with open(file_path, "r") as file:
-        content = file.read()
-        word_count = len(content.split())
+    with open(file_path, "r") as ex_file:
+        content = ex_file.read()
+        word_count = len(content.split(" "))
 
 
     output_path = UPLOAD_DIR / f"{file_path.stem}_word_count.txt"
     with open(output_path, "w") as f:
         f.write(f"Word Count: {word_count}")
     
-    background_tasks.add_task(process_file, file_path)
+    background_tasks.add_task(generate_progress_updates, file_id, db)
 
     if file_path.exists():
         file_path.unlink()
+
             
             
 @app.post("/upload/")
-async def upload_file(file: UploadFile, background_tasks: BackgroundTasks, status_code=status.HTTP_202_ACCEPTED, db = Session = Depends(get_session)):
-
-    task = FileModel(name=file.filename)
-    db.add(task)
+async def upload_file(file: UploadFile, background_tasks: BackgroundTasks, status_code=status.HTTP_202_ACCEPTED, db = Depends(get_session)):
+    file_id = str(uuid.uuid4())
+    filemodel = FileModel(name=file.filename, id=file_id)
+    db.add(filemodel)
     db.commit()
-    db.refresh(task)
+    db.refresh(filemodel)
 
     file_path = UPLOAD_DIR / file.filename
     with open(file_path, "w+b") as f:
         shutil.copyfileobj(file.file, f)
 
 
-    background_tasks.add_task(process_file, file_path)
-    raise status_code
+    background_tasks.add_task(process_file, file_path, filemodel.id, db, background_tasks)
+    return {"message": f"File {file.filename} is being proccessed in the background", "task_id": filemodel.id}
 
 
 @app.get("/tasks/{task_id}")
-def get_task_status(task_id: str, db: Session = Depends(get_session)):
-    task = db.exec(select(FileModel).where(FileModel.id == task_id)).first()
-    if not task:
+def get_task_status(file_id: str, db: Session = Depends(get_session)):
+    file = db.exec(select(FileModel).where(FileModel.id == file_id)).first()
+    if not file:
         raise HTTPException(status_code=404, detail="Task not found")
-    return {"task_id": task.id, "file_name": task.file_name}
+    return {"task_id": file.id, "file_name": file.name, "status": file.progress}
 
 
 
